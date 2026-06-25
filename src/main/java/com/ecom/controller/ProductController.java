aee14debcd9bbdde515b8ddc1ca645d82cf5e11d
MalikMedhat <malikmedhat77@gmail.com>
2026-06-25 23:32:04 +0300

    Update ProductController.java

--- src/main/java/com/ecom/controller/ProductController.java
+++ src/main/java/com/ecom/controller/ProductController.java
@@ -1,16 +1,18 @@
 package com.ecom.controller;
 
-
 import com.ecom.model.Product;
 import com.ecom.service.ProductService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:5174")
 public class ProductController {
+
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
